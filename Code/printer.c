d1e2e6ccb80dd32785192d0f5ee03f6ae37690ca
maxime <mux99@live.be>
2024-06-12 12:30:37 +0200

    change GPIO lib

--- Code/printer.c
+++ Code/printer.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#include <pigpio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <math.h>
+#include <wiringPi.h>
+
 
 #define m1_step 2
 #define m1_dir 3
@@ -22,68 +27,98 @@
 #define f5 19
 #define dclk 26
 
-#define stepper_angle
+#define steps_per_revolution 200
+
+struct Stepper {
+    int gpio_step;      //gpio pin 'step'
+    int gpio_direction; //gpio pin 'dir'
+    pid_t move_pid;
+};
+
+void servo_move(struct Stepper stepper, int speed_dps, int angle)
+{
+    int steps = (int)((abs(angle) / 360.0) * steps_per_revolution);
+    digitalWrite(stepper.gpio_direction, angle > 0);
+
+    // Calculate the step delay in microseconds
+    // Speed is given in degrees per second
+    float step_delay = (1.0 / ((speed_dps / 360.0) * steps_per_revolution)) * 1e6;
+
+    // Generate the pulses to move the stepper motor
+    for (int i = 0; i < steps; i++) {
+        digitalWrite(stepper.gpio_step, 1);
+        usleep(step_delay / 2);
+        digitalWrite(stepper.gpio_step, 0);
+        usleep(step_delay / 2);
+    }
+}
 
 int setup()
 {
-    if (gpioInitialise() < 0)
+    if (wiringPiSetupGpio() < 0)
     {
         fprintf(stderr, "pigpio initialisation failed\n");
         return -1;
     }
 
-    gpioSetMode(m1_step, PI_OUTPUT);
-    gpioWrite(m1_step, 0);
-    gpioSetMode(m1_dir, PI_OUTPUT);
-    gpioWrite(m1_dir, 0);
-    gpioSetMode(m2_step, PI_OUTPUT);
-    gpioWrite(m2_step, 0);
-    gpioSetMode(m2_dir, PI_OUTPUT);
-    gpioWrite(m2_dir, 0);
-    gpioSetMode(m3_step, PI_OUTPUT);
-    gpioWrite(m3_step, 0);
-    gpioSetMode(m3_dir, PI_OUTPUT);
-    gpioWrite(m3_dir, 0);
+    pinMode(m1_step, OUTPUT);
+    digitalWrite(m1_step, 0);
+    pinMode(m1_dir, OUTPUT);
+    digitalWrite(m1_dir, 0);
+    pinMode(m2_step, OUTPUT);
+    digitalWrite(m2_step, 0);
+    pinMode(m2_dir, OUTPUT);
+    digitalWrite(m2_dir, 0);
+    pinMode(m3_step, OUTPUT);
+    digitalWrite(m3_step, 0);
+    pinMode(m3_dir, OUTPUT);
+    digitalWrite(m3_dir, 0);
 
-    gpioSetMode(d1, PI_OUTPUT);
-    gpioWrite(d1, 0);
-    gpioSetMode(d2, PI_OUTPUT);
-    gpioWrite(d2, 0);
-    gpioSetMode(d3, PI_OUTPUT);
-    gpioWrite(d3, 0);
-    gpioSetMode(csync, PI_OUTPUT);
-    gpioWrite(csync, 0);
+    pinMode(d1, OUTPUT);
+    digitalWrite(d1, 0);
+    pinMode(d2, OUTPUT);
+    digitalWrite(d2, 0);
+    pinMode(d3, OUTPUT);
+    digitalWrite(d3, 0);
+    pinMode(csync, OUTPUT);
+    digitalWrite(csync, 0);
 
-    gpioSetMode(s1, PI_OUTPUT);
-    gpioWrite(s1,0);
-    gpioSetMode(s2, PI_OUTPUT);
-    gpioWrite(s2,0);
-    gpioSetMode(s3, PI_OUTPUT);
-    gpioWrite(s3,0);
-    gpioSetMode(s4, PI_OUTPUT);
-    gpioWrite(s4,0);
-    gpioSetMode(s5, PI_OUTPUT);
-    gpioWrite(s5,0);
-    gpioSetMode(f3, PI_OUTPUT);
-    gpioWrite(f3,0);
-    gpioSetMode(f5, PI_OUTPUT);
-    gpioWrite(f5,0);
-    gpioSetMode(dclk, PI_OUTPUT);
-    gpioWrite(dclk,0);
+    pinMode(s1, OUTPUT);
+    digitalWrite(s1,0);
+    pinMode(s2, OUTPUT);
+    digitalWrite(s2,0);
+    pinMode(s3, OUTPUT);
+    digitalWrite(s3,0);
+    pinMode(s4, OUTPUT);
+    digitalWrite(s4,0);
+    pinMode(s5, OUTPUT);
+    digitalWrite(s5,0);
+    pinMode(f3, OUTPUT);
+    digitalWrite(f3,0);
+    pinMode(f5, OUTPUT);
+    digitalWrite(f5,0);
+    pinMode(dclk, OUTPUT);
+    digitalWrite(dclk,0);
     
-}
-
-int test()
-{
-
-}
-
-int print()
-{
-
 }
 
 int main(int argc, char *argv[])
 {
+    if (setup() < 0) {
+        return 1;
+    }
+    
+    struct Stepper motor_picker;
+    motor_picker.gpio_direction = m1_dir;
+    motor_picker.gpio_step = m1_step;
+
+    struct Stepper motor_feeder;
+    motor_feeder.gpio_direction = m2_dir;
+    motor_feeder.gpio_step = m2_step;
+
+    struct Stepper motor_head;
+    motor_head.gpio_direction = m3_dir;
+    motor_head.gpio_step = m3_step;
+
 
 }
